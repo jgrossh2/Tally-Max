b5673a8ac1c48d5225a52dbdf5236b6170863b60
Ricky Bobby <richard.pajewski@gmail.com>
2020-10-13 13:05:59 -0500

    some pseudocode and some functions started

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+// Scrabble Word Generator
+
+// page elements
+
+// page variables
+var totalLetters = 0;
+var dictionary = [];
+ 
+fetch('/assets/dict/dict.txt').then(response => response.text()).then(data => { console.log(data)});
+// get user input
+// by drag and drop object
+
+// by user form
+function getInputValue() {
+    var letters = document.getElementById('possible-letters').value;
+    console.log(letters);
+    // get total letter count
+    letterCounter(letters);
+
+    function letterCounter(letters) {
+        // reset variable
+        totalLetters = 0;
+        var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var ar = alphabet.split("");
+        for (var i = 0; i < letters.length; i++) {
+            if (ar.indexOf(letters[i]) > -1) {
+                totalLetters = totalLetters + 1;
+            }
+        }
+        return totalLetters;
+    }
+    console.log(totalLetters);
+    // call word generator
+    genWordlist(totalLetters, letters);
+};
+
+// generate all possible combinations of inputted letters
+var genWordlist = function(totalLetters, letters) {
+    var results = [];
+
+    var generate = function(possWord) {
+        for (var i = 0; i < letters.length; i++) {
+        possWord += letters[i];
+        if (possWord.length === totalLetters) {
+            results.push(possWord);
+        } else {
+            generate(possWord);
+        }
+        possWord = possWord.slice(0, -1);
+        }
+    }
+    generate("");
+    return console.log(results);
+};
