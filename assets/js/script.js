4291b1de33f44630443f1ec2e427598454c212ca
MarynaPR <pryadkamaryna@gmail.com>
2020-10-13 20:31:23 -0500

    checking on merged files-practice

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@
 // page variables
 var totalLetters = 0;
 var dictionary = [];
- 
+
 // fetch('/assets/dict/dict.txt').then(response => response.text()).then(data => { console.log(data)});
 // get user input
 // by drag and drop object
@@ -35,20 +35,75 @@ function getInputValue() {
 };
 
 // generate all possible combinations of inputted letters
-var genWordlist = function(totalLetters, letters) {
+var genWordlist = function (totalLetters, letters) {
     var results = [];
 
-    var generate = function(possWord) {
+    var generate = function (possWord) {
         for (var i = 0; i < letters.length; i++) {
-        possWord += letters[i];
-        if (possWord.length === totalLetters) {
-            results.push(possWord);
-        } else {
-            generate(possWord);
-        }
-        possWord = possWord.slice(0, -1);
+            possWord += letters[i];
+            if (possWord.length === totalLetters) {
+                results.push(possWord);
+            } else {
+                generate(possWord);
+            }
+            possWord = possWord.slice(0, -1);
         }
     }
-    generate("");
+    generate("")
     return console.log(results);
 };
+//function that displays the words that are generated
+
+
+
+var wordDisplay = function () { //posWord or word you get form the dictionary?)
+
+    var word = document.querySelectore("wordEl").value;
+    var list = [];
+    localStorage.setItem("list", JSON.stringify(list));
+
+    var listOfWords = document.createElement("li");
+    //listofWords.classList.add("word generated")
+    var string = word;
+
+    listOfWords.textContent = string;
+
+    wordsGenerated.onclick = function () {
+        //    if ( )
+        //};
+
+        wordsGenerated.appendChild(listOfWords);
+
+        wordEl.setAttribute("class-name")
+
+        wordDefinition();
+        //placeholder
+    }
+
+    //words generating as a <li> w/ word on the left and on the right 2 clickable icons -1 to display the definition and 1 to display whatever else we choose
+
+    //fetch => the display of the object word
+    // var wordDefinition = function (word) {
+    //     console.log("icon");
+
+    //     var word = document.quesrySelector("#id").value;
+    //     //fetch
+        // fetch(
+        //     "http://api"
+        //         .then(function (response) ) {
+        //     console.log(response);
+        //     return response.json();
+
+        // })
+        //     .then(function (data) {
+        //         console.log(data);
+
+        //         el.setAttribute('src', "http:// " + data.list[i].el //'class-name');
+
+        //body.classList.add("card") if created in JS
+        //body.appendChild(body);
+    // })
+    //     wordDefinition('');
+  //  );
+    //API token is: b215d9b947a47ebd06cee1f48819e44474eeff9f
+    //curl --header "Authorization: Token b215d9b947a47ebd06cee1f48819e44474eeff9f" https://owlbot.info/api/v4/dictionary/owl -s | json_pp
