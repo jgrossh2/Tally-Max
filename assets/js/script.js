0ea9bccefef0dc8bf5035cef7c9d441ef8f66f75
Mona Lisa <joannagrosshans2@gmail.com>
2020-10-14 21:23:54 -0500

    js drop/drag to look at

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@
 // page variables
 var totalLetters = 0;
 var dropLetters = [];
-var letters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var letterEl = document.querySelector(".letter");
 
 // get user input
@@ -14,23 +14,14 @@ var dragLetters = function(event) {
     event.preventDefault();
     console.log("works")
 }
-//give letters data value 
-// var letterValue = function() {
-//     for (var i = 0; i < letters.length; i++){
-//         var letterBtn = $("<button>");
-//         letterBtn.attr("letters", letters[i]);
-//         letterBtn.addClass("submit");
-//         letterBtn.text(letters[i]);
-//         console.log("hello");
-//     };
-// }
-// by drag and drop object
+
+// make letters drag
 $(".letter").draggable({ 
     helper: "clone",
 });
-
-$(".space").droppable({
-    accept: ".button",
+//make dropzone
+$(".text").droppable({
+    accept: ".letter",
     tolerance: "touch",
     drop: function(event, ui) {
         console.log("drop");
@@ -41,15 +32,10 @@ $(".space").droppable({
     out: function(event, ui) {
         console.log("out");
     }
-
 });
 
-// letterValue();
 letterEl.addEventListener("click", dragLetters)
 
-//add data to letters
-//make letters draggable
-//make letters droppable
 // by user form
 function getInputValue() {
     var letters = document.getElementById('possible-letters').value;
