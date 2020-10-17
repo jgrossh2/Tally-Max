9d365ef3abe816029d7c1c2f9b6ff58fb42158e3
MarynaPR <pryadkamaryna@gmail.com>
2020-10-16 21:55:31 -0500

    fetch to show images

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,11 @@ function getInputValue() {
     genWordlist(totalLetters, letters);
 };
 
+
+// When the user clicks on a button generate a list of words and then put them on the page
+//for each word create a corresponding BUTTON with a info-ICON -API
+//and with IMAGE corresponding to the meaning of that word -API
+
 // generate all possible combinations of inputted letters
 var genWordlist = function (totalLetters, letters) {
     var results = [];
@@ -52,15 +57,16 @@ var genWordlist = function (totalLetters, letters) {
     return results
 };
 
-
-// When the user clicks on a button generate a list of words and then put them on the page
-//for each word create a corresponding BUTTON with a info-ICON -API
-//and with IMAGE corresponding to the meaning of that word -API
-
 //placeholder in html possibly?
 //words generating as a <li> w/ word on the left and on the right 2 clickable icons -1 to display the definition and 1 to display img
 
 //create function to show words in the list
+
+//cleate button function for modal Picture display(next step will be :get picAPI to display inside)
+//document.getElementById("images").addEventListener("click", showImage);
+
+
+
 var displayResults = function (results) {
     //check if there are any results
     if (results === 0) {
@@ -69,65 +75,34 @@ var displayResults = function (results) {
         //////////////////////////////////// 
         //shows results displayed in buttons:
 
-        // } else {
-        //     //totalLetters.textContent = document.getElementById("results").setAttribute("button", "onclick", results);
-        //     var just_five = [];
-        //     //create for loop to show 5 RANDOM words from the array
-        //     for (var i = 0; i < results.length; i++) {
-        //         just_five.push(results[i])
-        //         var wordDiv = document.createElement("button")
-        //         wordDiv.textContent = results[i]
-        //         resultsEl.appendChild(wordDiv)
-        //     }
+    } else {
+        //totalLetters.textContent = document.getElementById("results").setAttribute("button", "onclick", results);
+        var new_words = [];
+        //create for loop to show 5 RANDOM words from the array
+        for (var i = 0; i < results.length; i++) {
+            new_words.push(results[i])
+            var wordDiv = document.createElement("button")
+            wordDiv.textContent = results[i]
+            //console.log(results[i] + "test");
+            resultsEl.appendChild(wordDiv)
 
-        //     showDescription(just_five);
-        //     //showImage();
-        //     //showImage(just_five);
-        //     //getImg();
-        // }
+            wordDiv.onclick = function () {
 
+                //console.log(test);
+                showDescription(new_words);
+                showImage();
+                //getImg();
+            }
+        }
         return;
     }
-
-    var wordFromDictionaryEl = document.createElement('div');
-    wordFromDictionaryEl.classList.add("col-s6-m4-l3");
-    var text = results;
-    wordFromDictionaryEl.textContent = text;
-
-    var listGroupEl = document.querySelector(".results-list");
-    console.log(event.target)
-
-
-    listGroupEl.onclick = function () {
-
-
-        //create a container for each word
-        var wordEl = document.createElement('div');
-        wordEl.classList = 'list-item flex-row justify-space-between align-center';
-
-        // create a span element to hold word name
-        var newNameEl = document.createElement('span');
-        newNameEl.textContent = results;
-
-        // append to container
-        wordEl.appendChild(titleEl);
-
-        console.log(event.target.tagName)
-        if (event.target.tagName == "div") {
-            showDescription(event.target.textContent)
-            //getImg();
-            generateHTML(photos);
-
-        }
-    }
-    listGroupEl.appendChild(wordFromDictionaryEl);
-
 }
 
-//     //create a container for each word
-//create a span el to hold results by word displayed
+
+//new function for each displayed element?
 
 
+//document.querySelector("?button").addEventListener("click");
 var showDescription = function (word_array) {
 
     word_array.forEach(word => {
@@ -135,6 +110,7 @@ var showDescription = function (word_array) {
         var apiUrl = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=9197f1fd-982d-40cb-b0ef-a4e64d1afabb";
         //make a get request for url
         fetch(apiUrl)
+            // Convert the response to JSON
             .then(function (response) {
                 //request was sucessful
                 if (response.ok) {
@@ -145,13 +121,52 @@ var showDescription = function (word_array) {
                 } else {
                     return
                 }
+
             });
     })
 }
 
-//cleate button function for modal Picture display(next step will be :get picAPI to display inside)
-document.getElementById("images").addEventListener("click", showImage);
 var showImage = function () {
+
+    this.API_key = "563492ad6f91700001000001294e0c620d364f5597a8efd5b7667ccf";
+    var pexelURL = `https://api.pexels.com/v1/curated?query=${value}&per_page=10`;
+    var data = await fetchImages(pexelURL); //await and async used together
+    // this.generateHTML(data.photos) //photos is a data=an array from pexel in console log
+    console.log(data)//(response); //use 'awain in fetch function to wait for the results to load on page- get a response
+    //'await' goes together with 'async' -add to var
+
+    //add the function to fetch url, and call it above 
+    async function fetchImages(pexelURL) {
+        var response = await fetch(pexelURL, {
+            method: "GET", //there are 5 methods total to use if needed
+            headers: {
+                Accept: 'application/json',
+                Authorization: this.API_key
+            }
+        });
+        var data = await response.json();
+        // console.log(data); will display the array
+        return data; //return data and store it in var data above
+    }
+    //per pexel documentation, include sources and give credit to photographers
+    //either hardcode? or use display below the added info through classList.add
+    generateHTML(photos) {
+        photos.forEach(photo => {//photos in here refers to data in array from console log, when using another object- change to that
+            //create var for instead of a div in html that <div class="item" for example
+            var item = document.createElement("div");
+            //add class
+            item.classList.add("item");
+            //string 
+            item.innerHTML = `
+            <a href="#">
+             <img src="${photo.src.large}">
+             <h4>${photo.photographer}</h4>
+             </a>
+             `;//from array of objects- change if needed a dif source displayed
+            //append
+            this.imagesDiv.appendChild(item);
+        })
+    }
     document.getElementById("images").innerHTML = "Image";
 }
 
@@ -176,7 +191,7 @@ class Images {
     }
     async getImg() {
         //link from PEXEL for search pic: "https://api.pexels.com/v1/search?query=nature&per_page=1"
-        var pexelURL = "https://api.pexels.com/v1/search?query=${results}&per_page=1";
+        var pexelURL = "https://api.pexels.com/v1/curated?query=${new-words}&per_page=10";
         var data = await this.fetchImages(pexelURL); //await and async used together
         this.generateHTML(data.photos) //photos is a data=an array from pexel in console log
         console.log(data)//(response); //use 'awain in fetch function to wait for the results to load on page- get a response
@@ -198,7 +213,7 @@ class Images {
     //per pexel documentation, include sources and give credit to photographers
     //either hardcode? or use display below the added info through classList.add
     generateHTML(photos) {
-        photos.forEach(photo => {//photos in here refers to data in array from console log, when using another object-make sure to change to that
+        photos.forEach(photo => {//photos in here refers to data in array from console log, when using another object- change to that
             //create var for instead of a div in html that <div class="item" for example
             var item = document.createElement("div");
             //add class
@@ -206,7 +221,7 @@ class Images {
             //string 
             item.innerHTML = `
             <a href="#">
-             <img src="${photo.src.medium}">
+             <img src="${photo.src.large}">
              <h4>${photo.photographer}</h4>
              </a>
              `;//from array of objects- change if needed a dif source displayed
