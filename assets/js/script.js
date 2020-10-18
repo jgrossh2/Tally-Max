22707100c488480a889b2e62bafd7113fc27ba05
Ricky Bobby <richard.pajewski@gmail.com>
2020-10-18 15:39:41 -0500

    both fetches combine to one object

--- assets/js/script.js
+++ assets/js/script.js
@@ -275,66 +275,27 @@ var getDefData = function (letters, results) {
             }))
             // word definition
             .then(function (response) {
-                console.log(response);
-                if (response.ok) {
-                        console.log(data);
-                        var wordDef = data[0];
-                        var imgSrc = data[1];
-                        
-                        console.log(wordDef);
-                        console.log(imgSrc);
+                var wordDef = response[0];
+                var imgSrc = response[1];
 
-                        var def = (data[0])
-                        var wordDefArr = {
-                            word: word,
-                            class: def.fl,
-                            definition: def.shortdef,
-                            audio: def.hwi.prs[0].sound.audio,
-                            offensive: def.meta.offensive,
-                        };
-                        console.log(wordDefArr)
-                        // displayWordDefSound(wordDef)
-                        // displaySoundBite(wordDef
-                        return wordDefArr
-                    
-                } else {
-                    alert("Error:" + response.statusText)
-                }
+                var def = (wordDef[0])
+                var wordDefArr = {
+                    word: word,
+                    class: def.fl,
+                    definition: def.shortdef,
+                    audio: def.hwi.prs[0].sound.audio,
+                    offensive: def.meta.offensive,
+                    imageInfo: imgSrc.photos,
+                };
+                console.log(wordDefArr)
+                displayWord(wordDefArr);
+                return wordDefArr
             })
+                // } else {
+                //     alert("Error:" + response.statusText)
+                // }
         });
     };
-
-    // for (var i = 0; i < results.length; i++) {
-    //     var images = results[i];
-    //     var pexelURL = `https://api.pexels.com/v1/search?query=${images}&per_page=1`;
-    //     var API_key = "563492ad6f91700001000001294e0c620d364f5597a8efd5b7667ccf";
-    //     //add the function to fetch url, and call it above 
-    //     fetch(pexelURL, {
-    //         headers: {
-    //             // Accept: 'application/json',
-    //             Authorization: API_key
-    //             //credentials: 'include'
-    //         }
-    //     })
-    //         .then(function (response) {
-    //             return response.json();
-    //         })
-    //         .then(function (response) {
-    //             var imgSrcInfo = response.photos;
-    //             // console.log(imgSrcInfo);
-    //             // console.log(response.photos);
-    //             // // Use 'querySelector' to get the ID of where the pic/ will be displayed
-    //             // var responseContainerEl = document.querySelector('#images');
-    //             // // // Create an '<img>' element
-    //             // var pexelImg = document.createElement('img');
-    //             // // Set that element's 'src' attribute to the 'image_url' from API response
-    //             // pexelImg.setAttribute('src', response.photos[0].src.small);
-    //             // responseContainerEl.appendChild(pexelImg);
-    //             imgSrcInfoArr.push(imgSrcInfo);
-    //             // console.log(imgSrcInfo);
-    //             // return imgSrcInfo;
-    //         })
-    // }
 };
 
 // function takes MW api object data and packages word & class (e.g. noun, verb adjective) for DOM object display
@@ -399,85 +360,4 @@ var displayWord = function (wordDefArr) {
     } else {
         console.log("Sorry, this word cannot be displayed.");
     }
-};
-
-    // var showImage = function () {
-    //     var pexelURL = `https://api.pexels.com/v1/search?query=${wordDef[0]}&per_page=1`;// ${new_words[0]}
-    //     var API_key = "563492ad6f91700001000001294e0c620d364f5597a8efd5b7667ccf";
-    //     //add the function to fetch url, and call it above 
-    //     fetch(pexelURL, {
-    //         headers: {
-    //             // Accept: 'application/json',
-    //             Authorization: API_key
-    //             //credentials: 'include'
-    //         }
-    //     })
-    //         .then(function (response) {
-    //             console.log(response);
-    //             return response.json();
-    //         })
-    //         //console.log(response);// will display the array
-    //         .then(function (response) {
-    //             console.log(response.photos);
-    //             // Use 'querySelector' to get the ID of where the pic/ will be displayed
-    //             var responseContainerEl = document.querySelector('#images');
-    //             // // Create an '<img>' element
-    //             var pexelImg = document.createElement('img');
-    //             // Set that element's 'src' attribute to the 'image_url' from API response
-    //             pexelImg.setAttribute('src', response.photos[0].src.small);
-    //             responseContainerEl.appendChild(pexelImg);
-
-
-    //             // Pexel credit
-    //             // var pexelCreditEl = document.createElement('div')
-    //             // pexelCreditEl.classList.add("card-body");
-    //             // var pexelInfoDiv = document.createElement('div');
-    //             // pexelInfoDiv.classList.add("card");
-    //             // var photographerEl = document.createElement("p");
-    //             // photographerEl.textContent = "Photo by: ";
-    //             // photographerEl.classList.add("card-text");
-    //             // var madeByPexelEl = document.createElement("p");
-    //             // madeByPexelEl.textContent = "Photos provided by Pexels";
-    //             // madeByPexelEl.classList.add("card-text");
-    //             // var logoLinkEl = document.createElement("p");
-    //             // logoLinkEl.textContent = "";//''"<a href="https://www.pexels.com"><img src="https://images.pexels.com/lib/api/pexels.png"/></a>';
-    //             // logoLinkEl.classList.add("card-text");
-    //             //pexelInfoDiv.setAttribute('src', response.photographer);
-    //             // console.log(headers.url);
-    //             //pexelInfoDiv.appendChild(pexelCreditEl);
-    //             //     pexelCreditEl.appendChild(photographerEl);
-    //             //     pexelCreditEl.appendChild(madeByPexelEl);
-    //             //     pexelCreditEl.appendChild(logoLinkEl);
-    //             //     pexelCreditEl.appendChild(pexelInfoDiv);})
-    //         })
-    // }
-    // document.getElementById("images").innerHTML = "Image";
-
-    // FETCH FUNCTION formats and sends api request
-var getWeather = function (userCity) {
-    // format api urls for both OpenWeather endpoints in order to make multiple api calls at once using promise.all() and array.map() methods learned from the following site: https://gomakethings.com/waiting-for-multiple-all-api-responses-to-complete-with-the-vanilla-js-promise.all-method/
-    var apiUrls = [
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&units=imperial&appid=e4c79656912e2022efd4f848cf4c49dc"),
-        fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + userCity + "&units=imperial&appid=e4c79656912e2022efd4f848cf4c49dc"),
-    ];
-    // submit https request
-    Promise.all(apiUrls).then(function (responses) {
-        // using map() method to get a response array of json objects, 
-        return Promise.all(responses.map(function (response) {
-            return response.json();
-        }))
-            .then(function (data) {
-                console.log(data);
-                cityStats = data[0];
-                forecast = data[1];
-                console.log(cityStats);
-                console.log(forecast);
-                displayCityStats(cityStats)
-                displayForecast(forecast)
-            })
-            // catch alert to catch any network errors
-            .catch(function (error) {
-                console.log(error);
-            });
-    });
 };
