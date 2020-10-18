4482982725fbe28757b59b8bbe2afb1bc4bb0388
Mona Lisa <joannagrosshans2@gmail.com>
2020-10-18 15:51:01 -0500

    need pull update

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,67 +21,58 @@ var dragLetters = function(event) {
     console.log("works")
 }
 
-$(".letter").sortable({
-    revert: true
-});
+// $("#sortable").sortable({
+//     revert: true,
+//     axis: "x",
+// });
 // make letters drag
 $(".letter").draggable({ 
-    connectToSortable: ".space",
+    // connectToSortable: ".space",
     tolerance: "pointer",
     helper: "clone",
     appendTo: ".space",
-    containment: "#keyboard",
+    containment: "#container",
     cursor: "move",
     snap: ".space",
+    // snapMode: "inner",
     revert: "invalid",
     start: function(event, ui) {
-        console.log(ui);
+        console.log("uivalue " + JSON.stringify(ui));
         //clone of tile
         $(ui.helper).addClass("dragging");
         console.log("test");
+        $(this).addClass("gray");
     },
     stop: function(event, ui) {
         $(ui.helper).removeClass("dragging");
         console.log("stop");
-        // var grid = document.createElement("div");
-        // grid.id = "grid";
-        // grid.className = "grid";
-        // for (i=0; i<7; i++) {
-        //     var row = grid.appendChild(document.createElement("div"));
-        //     row.className = "row";
-        //     row.id = "row" +i;
-        //     console.log("well?");
-        // };
     }
-    //remove class
-    // add grid item
 });
 
-// $(".letter").sortable({
-//     revert: true
-// });
-//     connectWith: $(".space .letter"),
-//     tolerance: "pointer",
-//     helper: "clone",
-//     activate: function(event) {
-//         var letterVal= $(this).attr("data-letter");
-//             dropLetters.push(letterVal);
-//             console.log(letterVal);
-        // $(".space").addClass("dropZone");
-        // console.log("activate", this);
+$((".space").children).sortable({
+    revert: true,
+    placeholder: "highlight",
+    axis: "x",
+    connectWith: $(".dropped"),
+    tolerance: "pointer",
+    helper: "clone",
+    appendTo: ".space",
+    // start: function(event) {
+    //     ui.helper.toggleClass("highlight");
     //   },
-//       deactivate: function(event) {
-//         // $(".bottom-trash").removeClass("dropover bottom-trash-drag");
-//         console.log("deactivate", this);
-//       },
-//       over: function(event) {
-//         // $(event.target).addClass("dropover-active");
-//       },
-//       out: function(event) {
-//         // $(event.target).removeClass("dropover-active");
-//         console.log("out", event.target);
-//       },
-// })
+    //   stop: function(event) {
+    //       ui.helper.toggleClass("highlight");
+    //     // $(".bottom-trash").removeClass("dropover bottom-trash-drag");
+    //     console.log("deactivate", this);
+    //   },
+    //   over: function(event) {
+    //     // $(event.target).addClass("dropover-active");
+    //   },
+    //   out: function(event) {
+    //     // $(event.target).removeClass("dropover-active");
+    //     console.log("out", event.target);
+    //   },
+})
 //pull tiles into dropzone if overlap
 
 //make dropzone
@@ -95,8 +86,8 @@ $(".space").droppable({
         var helper = ui.helper.clone(true);
         helper.appendTo(".space");
         $(ui.helper).removeClass("dragging");
+        // $(".letter").draggable('disable');
         // finds object and then letter value of that object
-
         var dragged= ui.draggable[0].dataset.letter;
         console.log(ui.draggable[0].dataset.letter);
         //add drop letters to array
@@ -118,7 +109,8 @@ $(".space").droppable({
     }
 });
 
-//only accept so many of each letter
+// grays out letter on drop, find ids and match
+
 
 letterEl.addEventListener("click", dragLetters)
 
