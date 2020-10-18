f447fe0a78744dec856a376eb779db4c11622f21
MarynaPR <pryadkamaryna@gmail.com>
2020-10-18 14:17:19 -0500

    modal added with style colors and some text for testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -382,55 +382,79 @@ var displayWordDefSound = function (defObject) {
 
 };
 
-    // var showImage = function () {
-    //     var pexelURL = `https://api.pexels.com/v1/search?query=${wordDef[0]}&per_page=1`;// ${new_words[0]}
-    //     var API_key = "563492ad6f91700001000001294e0c620d364f5597a8efd5b7667ccf";
-    //     //add the function to fetch url, and call it above 
-    //     fetch(pexelURL, {
-    //         headers: {
-    //             // Accept: 'application/json',
-    //             Authorization: API_key
-    //             //credentials: 'include'
-    //         }
-    //     })
-    //         .then(function (response) {
-    //             console.log(response);
-    //             return response.json();
-    //         })
-    //         //console.log(response);// will display the array
-    //         .then(function (response) {
-    //             console.log(response.photos);
-    //             // Use 'querySelector' to get the ID of where the pic/ will be displayed
-    //             var responseContainerEl = document.querySelector('#images');
-    //             // // Create an '<img>' element
-    //             var pexelImg = document.createElement('img');
-    //             // Set that element's 'src' attribute to the 'image_url' from API response
-    //             pexelImg.setAttribute('src', response.photos[0].src.small);
-    //             responseContainerEl.appendChild(pexelImg);
+// var showImage = function () {
+//     var pexelURL = `https://api.pexels.com/v1/search?query=${wordDef[0]}&per_page=1`;// ${new_words[0]}
+//     var API_key = "563492ad6f91700001000001294e0c620d364f5597a8efd5b7667ccf";
+//     //add the function to fetch url, and call it above 
+//     fetch(pexelURL, {
+//         headers: {
+//             // Accept: 'application/json',
+//             Authorization: API_key
+//             //credentials: 'include'
+//         }
+//     })
+//         .then(function (response) {
+//             console.log(response);
+//             return response.json();
+//         })
+//         //console.log(response);// will display the array
+//         .then(function (response) {
+//             console.log(response.photos);
+//             // Use 'querySelector' to get the ID of where the pic/ will be displayed
+//             var responseContainerEl = document.querySelector('#images');
+//             // // Create an '<img>' element
+//             var pexelImg = document.createElement('img');
+//             // Set that element's 'src' attribute to the 'image_url' from API response
+//             pexelImg.setAttribute('src', response.photos[0].src.small);
+//             responseContainerEl.appendChild(pexelImg);
 
 
-    //             // Pexel credit
-    //             // var pexelCreditEl = document.createElement('div')
-    //             // pexelCreditEl.classList.add("card-body");
-    //             // var pexelInfoDiv = document.createElement('div');
-    //             // pexelInfoDiv.classList.add("card");
-    //             // var photographerEl = document.createElement("p");
-    //             // photographerEl.textContent = "Photo by: ";
-    //             // photographerEl.classList.add("card-text");
-    //             // var madeByPexelEl = document.createElement("p");
-    //             // madeByPexelEl.textContent = "Photos provided by Pexels";
-    //             // madeByPexelEl.classList.add("card-text");
-    //             // var logoLinkEl = document.createElement("p");
-    //             // logoLinkEl.textContent = "";//''"<a href="https://www.pexels.com"><img src="https://images.pexels.com/lib/api/pexels.png"/></a>';
-    //             // logoLinkEl.classList.add("card-text");
-    //             //pexelInfoDiv.setAttribute('src', response.photographer);
-    //             // console.log(headers.url);
-    //             //pexelInfoDiv.appendChild(pexelCreditEl);
-    //             //     pexelCreditEl.appendChild(photographerEl);
-    //             //     pexelCreditEl.appendChild(madeByPexelEl);
-    //             //     pexelCreditEl.appendChild(logoLinkEl);
-    //             //     pexelCreditEl.appendChild(pexelInfoDiv);})
-    //         })
-    // }
-    // document.getElementById("images").innerHTML = "Image";
+//             // Pexel credit
+//             // var pexelCreditEl = document.createElement('div')
+//             // pexelCreditEl.classList.add("card-body");
+//             // var pexelInfoDiv = document.createElement('div');
+//             // pexelInfoDiv.classList.add("card");
+//             // var photographerEl = document.createElement("p");
+//             // photographerEl.textContent = "Photo by: ";
+//             // photographerEl.classList.add("card-text");
+//             // var madeByPexelEl = document.createElement("p");
+//             // madeByPexelEl.textContent = "Photos provided by Pexels";
+//             // madeByPexelEl.classList.add("card-text");
+//             // var logoLinkEl = document.createElement("p");
+//             // logoLinkEl.textContent = "";//''"<a href="https://www.pexels.com"><img src="https://images.pexels.com/lib/api/pexels.png"/></a>';
+//             // logoLinkEl.classList.add("card-text");
+//             //pexelInfoDiv.setAttribute('src', response.photographer);
+//             // console.log(headers.url);
+//             //pexelInfoDiv.appendChild(pexelCreditEl);
+//             //     pexelCreditEl.appendChild(photographerEl);
+//             //     pexelCreditEl.appendChild(madeByPexelEl);
+//             //     pexelCreditEl.appendChild(logoLinkEl);
+//             //     pexelCreditEl.appendChild(pexelInfoDiv);})
+//         })
+// }
+// document.getElementById("images").innerHTML = "Image";
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
